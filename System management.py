2714f96e2edfa4324a768b8da9666493245ba26d
Artem <arteminkin@yandex.ru>
2022-12-04 03:26:37 +0400

    Рабочая версия с хорошим функционалом

--- System management.py
+++ System management.py
@@ -1,56 +1,52 @@
 import os
 import subprocess
-import pyautogui as pag
-import time
-import sys
 
+commands = ("off", "reboot", "clone", "update", "exit" )
 
-commands = ("off", "reboot", "update" )
-print("\nВведите команду или help для просмотра доступных команд: ")
-ans = input()
+state = True   # выключатель, может понадобиться в будущем
+while(state):
 
-if (ans == "help"):
-    print("\nДоступные команды:")
-    for i in range (len(commands)):
-        print(commands[i])
+    print("\nВведите команду или help для просмотра доступных команд: ")
+    ans = input()
 
-elif (ans == "off"):
-    os.system('shutdown /p /f')  # sudo shutdown -h now на линуксе
+    if (ans == "help"):
+        print("\nДоступные команды:")
+        for i in range (len(commands)):
+            print(commands[i])
 
-elif (ans == "reboot"):
-    os.system('shutdown -r -t 0')
+    elif (ans == "off"):
+        os.system('systemctl poweroff')  # os.system('shutdown /p /f') на винде
 
-elif (ans == "0"):
-    #way = input("\nВведите путь локального репозитория: ")
-    #x = subprocess.Popen(['start', 'cmd'], shell=True)
-    #x.wait()
+    elif (ans == "reboot"):
+        subprocess.check_call('reboot')   # os.system('shutdown -r -t 0') на винде
 
-    #p2 = subprocess.run(["F:/GIIIIIIIT", "-c", "print('ocean')"])
-    #os.system("cd F:")
-    #os.system('cd "F:\Git123"')
-    #res = subprocess.run('ls')
+    elif (ans == "clone"):
+        way = input("\nВведите путь для копирования репозитория: ")
+        link = input("\nВведите ссылку на репозиторий GitHub: ")
+        cmd = "git clone " + link
+        
+        os.chdir(way)
+        p = subprocess.run(cmd, shell=True)
 
-    #p1.wait()
+        if p.returncode == 0:
+            print("\nУспех!!!")
+        else: 
+            print("\nЧто то пошло не так :(")
 
-    cmd2 = "git clone https://github.com/Parcif/Line_deleter"
-    cmd= "git pull"
-    #p2 = subprocess.run("cd C:\\Users\\Artem1\\Desktop\\testim", shell=True)
+    elif(ans == "update"):
+        way = input("\nВведите путь для обновления репозитория: ")
+        cmd = "git pull"
 
-    os.chdir("C:\\Users\\Artem1\\Desktop\\Line_deleter")
-    p3 = subprocess.run(cmd, shell=True)
+        os.chdir(way)
+        p = subprocess.run(cmd, shell=True)
 
-    if p3.returncode==0:
-        print("command success")
-    #time.sleep(1)
-    '''pag.typewrite("cd " + way)
-    pag.press('enter')
-    pag.typewrite("git push origin master")
-    pag.press('enter')'''
+        if p.returncode == 0:
+            print("\nУспех!!!")
+        else: 
+            print("\nЧто то пошло не так :(")
 
-else:
-    print("\nНеизвеcтная команда!")
+    elif(ans == "exit"):
+        state = False
 
-
-
-
-
+    else:
+        print("\nНеизвеcтная команда!!!\n")
